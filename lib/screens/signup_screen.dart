c936dc3b018793707869c5ce0e308c6f9aabb02f
aydinmehmetg <aydinmehmet.g@gmail.com>
2019-10-19 04:19:34 +0300

    firebase bağlantı yapılacak küçük düzenlemeler

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -23,94 +23,96 @@ class _SinupScreenState extends State<SinupScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'PollApp',
-              style: TextStyle(
-                  fontSize: 50.0,
-                  fontFamily: 'SEGA',
-                  color: Color.fromRGBO(0, 191, 255, 50)),
-            ),
-            Form(
-              key: _formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                   Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 30.0,
-                      vertical: 10.0,
-                    ),
-                    child: TextFormField(
-                      decoration: InputDecoration(labelText: 'Name'),
-                      validator: (input) => input.trim().isEmpty
-                          ? 'İsim Alanı Boş Geçilemez.'
-                          : null,
-                      onSaved: (input) => _name = input,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 30.0,
-                      vertical: 10.0,
-                    ),
-                    child: TextFormField(
-                      decoration: InputDecoration(labelText: 'E-Mail'),
-                      validator: (input) => !input.contains('@')
-                          ? 'lütfen Geçerli Bir E-Mail Adresi Giriniz.'
-                          : null,
-                      onSaved: (input) => _password = input,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 30.0,
-                      vertical: 10.0,
-                    ),
-                    child: TextFormField(
-                      decoration: InputDecoration(labelText: 'Password'),
-                      validator: (input) =>
-                          input.length < 8 ? 'en az 8 karakter olmalı..' : null,
-                      onSaved: (input) => _email = input,
-                      obscureText: true,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  Container(
-                    width: 200.0,
-                    child: FlatButton(
-                      onPressed: _sumbit,
-                      color: Color.fromRGBO(0, 191, 255, 50),
-                      child: Text(
-                        "Singup",
-                        style: TextStyle(color: Colors.white, fontSize: 18.0),
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 30.0,
-                  ),
-                  Container(
-                    width: 200.0,
-                    child: FlatButton(
-                      onPressed: () => Navigator.pop(context),
-                      color: Color.fromRGBO(0, 191, 255, 50),
-                      child: Text(
-                        "Go To Login",
-                        style: TextStyle(color: Colors.white, fontSize: 18.0),
-                      ),
-                    ),
-                  ),
-                ],
+      body: SingleChildScrollView (
+                child: Container(height: MediaQuery.of(context).size.height,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'PollApp',
+                style: TextStyle(
+                    fontSize: 50.0,
+                    fontFamily: 'SEGA',
+                    color: Color.fromRGBO(0, 191, 255, 50)),
               ),
-            )
-          ],
+              Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                     Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 30.0,
+                        vertical: 10.0,
+                      ),
+                      child: TextFormField(
+                        decoration: InputDecoration(labelText: 'Name'),
+                        validator: (input) => input.trim().isEmpty
+                            ? 'İsim Alanı Boş Geçilemez.'
+                            : null,
+                        onSaved: (input) => _name = input,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 30.0,
+                        vertical: 10.0,
+                      ),
+                      child: TextFormField(
+                        decoration: InputDecoration(labelText: 'E-Mail'),
+                        validator: (input) => !input.contains('@')
+                            ? 'lütfen Geçerli Bir E-Mail Adresi Giriniz.'
+                            : null,
+                        onSaved: (input) => _password = input,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 30.0,
+                        vertical: 10.0,
+                      ),
+                      child: TextFormField(
+                        decoration: InputDecoration(labelText: 'Password'),
+                        validator: (input) =>
+                            input.length < 8 ? 'en az 8 karakter olmalı..' : null,
+                        onSaved: (input) => _email = input,
+                        obscureText: true,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Container(
+                      width: 200.0,
+                      child: FlatButton(
+                        onPressed: _sumbit,
+                        color: Color.fromRGBO(0, 191, 255, 50),
+                        child: Text(
+                          "Singup",
+                          style: TextStyle(color: Colors.white, fontSize: 18.0),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30.0,
+                    ),
+                    Container(
+                      width: 200.0,
+                      child: FlatButton(
+                        onPressed: () => Navigator.pop(context),
+                        color: Color.fromRGBO(0, 191, 255, 50),
+                        child: Text(
+                          "Go To Login",
+                          style: TextStyle(color: Colors.white, fontSize: 18.0),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
